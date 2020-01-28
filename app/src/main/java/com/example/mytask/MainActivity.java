d1ec7f65aa39c574c736727de9074febc80697c7
petersamuel10 <petersamuelyakoub@gmail.com>
2020-01-29 02:14:08 +0300

    AppBar

---
+++ app/src/main/java/com/example/mytask/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mytask;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView restaurantRc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Restaurants");
+        restaurantRc = findViewById(R.id.restaurant_rc);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.tool_bar_menu, menu);
+        return true;
+    }
+}
